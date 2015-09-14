7d3654cf9e0ed60da193b0cd79de5b06247de55c
Mathijs Bernson <mathijs.bernson@gmail.com>
2015-09-14 14:51:16 +0200

    Complete the specifications, add comments

--- src/main/java/itest/consensus/impl/ConcensusImpl.java
+++ src/main/java/itest/consensus/impl/ConcensusImpl.java
@@ -8,13 +8,18 @@ import java.util.ArrayList;
 public class ConcensusImpl implements Concensus {
     @Override
     public boolean voting(ArrayList<Voter> voters) {
+        if(null == voters || voters.isEmpty())
+            throw new RuntimeException("Cannot complete the voting because there are no voters.");
+
+        boolean result = true;
+
         for(final Voter v : voters) {
             if(null == v)
                 continue;
 
             if(false == v.vote())
-                return false;
+                result = false;
         }
-        return true;
+        return result;
     }
 }
--- src/test/java/itest/consensus/ConcensusTest.java
+++ src/test/java/itest/consensus/ConcensusTest.java
@@ -2,6 +2,7 @@ package itest.consensus;
 
 import itest.consensus.impl.ConcensusImpl;
 import itest.consensus.impl.voter.FalseVoter;
+import itest.consensus.impl.voter.RandomVoter;
 import itest.consensus.impl.voter.TrueVoter;
 import org.junit.After;
 import org.junit.Before;
@@ -22,13 +23,21 @@ public class ConcensusTest {
         consensus = new ConcensusImpl();
     }
 
+    /**
+     * Specificatie 1: De returnvalue van voting is alleen false als minimaal één stemmer als stem false geeft.
+     */
     @Test
     public void testVotingReturnsFalseWhenOneVoterVotesFalse() {
+        voters.add(new TrueVoter());
         voters.add(new FalseVoter());
+        voters.add(new RandomVoter());
 
         assertFalse(consensus.voting(voters));
     }
 
+    /**
+     * Specificatie 2: Als alle stemgerechtigden true stemmen dan moet de returnvalue van voting() true zijn.
+     */
     @Test
     public void testVotingReturnsTrueWhenAllVotersVoteTrue() {
         voters.add(new TrueVoter());
@@ -38,26 +47,52 @@ public class ConcensusTest {
         assertTrue(consensus.voting(voters));
     }
 
+    /**
+     * Specificatie 3: Als specificatie 1 en 2 zich niet voordoen moet er een exception worden opgeworpen.
+     */
+    @Test(expected = Exception.class)
+    public void testExceptionIsThrownWhenNobodyVoted() {
+        consensus.voting(voters);
+    }
+
+    /**
+     * Specificatie 4: Iedere valide stemmer (dat wil zeggen, iedere niet null stemmer) moet stemmen.
+     */
     @Test
-    public void testVoteIsCalledExactlyOnce() {
+    public void testConcencusIgnoresNullVoters() {
         Voter voterMock = mock(Voter.class);
 
-        when(voterMock.vote()).thenReturn(false);
-
+        voters.add(new TrueVoter());
+        voters.add(null);
+        voters.add(new FalseVoter());
         voters.add(voterMock);
+        voters.add(null);
 
-        consensus.voting(voters);
+        assertFalse(consensus.voting(voters));
 
         verify(voterMock).vote();
     }
 
+    /**
+     * Specificatie 5: Geen stemgerechtigde mag vaker dan 1 keer stemmen.
+     *
+     * TODO: What is the same Voter object appears in the list multiple times?
+     */
     @Test
-    public void testConcencusIgnoresNullVoters() {
-        voters.add(new TrueVoter());
-        voters.add(null);
-        voters.add(new FalseVoter());
+    public void testVoteIsCalledExactlyOnce() {
+        Voter voterMockFalse = mock(Voter.class),
+              voterMockTrue = mock(Voter.class);
 
-        assertFalse(consensus.voting(voters));
+        when(voterMockFalse.vote()).thenReturn(false);
+        when(voterMockTrue.vote()).thenReturn(false);
+
+        voters.add(voterMockFalse);
+        voters.add(voterMockTrue);
+
+        consensus.voting(voters);
+
+        verify(voterMockFalse).vote();
+        verify(voterMockTrue).vote();
     }
 
     @After
